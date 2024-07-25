2cd5df175d76c602067c6115f67d676e1e21a6ec
bipulsardar421 <bipulsardar421@gmail.com>
2024-07-25 17:02:29 +0530

    __init__

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include "db.php"
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="main-div">
+        <div class="inside-main">
+            <div class="all-labels">
+                <label>First Name</label>
+                <label>Last Name</label>
+                <label>Email</label>
+                <label>Password</label>
+            </div>
+            <div class="all-input">
+                <input type="text" placeholder="fname">
+                <input type="text" placeholder="lname">
+                <input type="text" placeholder="email">
+                <input type="text" placeholder="pwd">
+            </div>
+            <div class="sign_btn">
+            <button>
+                Sign Up
+            </button>
+        </div>
+        </div>
+  
+    </div>
+</body>
+
+</html>
